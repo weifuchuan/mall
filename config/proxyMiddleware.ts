cd263f57f2e008dc74a06ca5a9a4d373fc3d1e33
wei fuchuan <fuchuan.wei@hotmail.com>
2018-12-02 11:38:02 +0800

    init

---
+++ config/proxyMiddleware.ts
@@ -0,0 +1,14 @@
+import proxy from 'http-proxy-middleware'
+
+export default undefined; 
+
+/*
+export default proxy('/api/', {
+  target: "http://localhost:80",
+  changeOrigin: true,
+  ws: true,
+  pathRewrite: function (path, req) {
+    return path.substring(4); // trim left /api
+  }
+});
+*/
---
+++ src/index/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.scss';
+
+interface Props {
+	
+}
+
+
+export default class Home extends React.Component<Props> {
+	
+
+	render() {
+		return (
+			<div className="HomeContainer">
+			
+			</div>
+		);
+	}
+}
